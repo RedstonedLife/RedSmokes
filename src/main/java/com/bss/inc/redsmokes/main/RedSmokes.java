a58fea3034b2ca889619c2f19824b0d0d40a445e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 06:09:14 +0300

    Update-8bf9a627-9f48-4acb-9613-eceab0288ff5

--- src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
+++ src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -2,6 +2,7 @@ package com.bss.inc.redsmokes.main;
 
 import com.bss.inc.redsmokes.api.IRedSmokes;
 import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,6 +24,6 @@ public class RedSmokes extends JavaPlugin implements IRedSmokes {
     }
 
     public <T> RegisteredServiceProvider<T> getServiceProvider(Class<T> clazz) {
-        return 
+        return Bukkit.getServicesManager().getRegistration(clazz);
     }
 }
