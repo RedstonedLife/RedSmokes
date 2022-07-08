210a66113a2f077df47e517acbce0661d3dc2000
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 06:07:10 +0300

    Update-120968f4-c1a5-45f5-8221-b5376d1854c4

--- src/main/java/com/bss/inc/redsmokes/main/apis/LuckPermsAPI.java
+++ src/main/java/com/bss/inc/redsmokes/main/apis/LuckPermsAPI.java
@@ -1,6 +1,21 @@
 package com.bss.inc.redsmokes.main.apis;
 
 import com.bss.inc.redsmokes.api.apis.ILuckPerms;
+import net.luckperms.api.LuckPerms;
 
 public class LuckPermsAPI implements ILuckPerms {
+
+    private LuckPerms
+
+    public LuckPerms onStart() {
+
+    }
+
+    public void onStop() {
+
+    }
+
+    public boolean isLoaded() {
+        return false;
+    }
 }
