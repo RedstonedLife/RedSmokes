5513d147c3975f1a36670583f4104dc5c7e6c3cc
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 06:10:21 +0300

    Update-8296f098-7b35-473d-a4a7-2a7c272ce70f

--- src/main/java/com/bss/inc/redsmokes/main/apis/LuckPermsAPI.java
+++ src/main/java/com/bss/inc/redsmokes/main/apis/LuckPermsAPI.java
@@ -3,11 +3,13 @@ package com.bss.inc.redsmokes.main.apis;
 import com.bss.inc.redsmokes.api.apis.ILuckPerms;
 import com.bss.inc.redsmokes.main.RedSmokes;
 import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class LuckPermsAPI implements ILuckPerms {
 
     private LuckPerms luckPerms;
+    private static final RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
 
     public LuckPerms onStart() {
         if(RedSmokes.getProvidingPlugin(LuckPerms.class) != null) {
