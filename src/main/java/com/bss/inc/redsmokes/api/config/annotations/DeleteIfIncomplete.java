00e6a9f1912d9ebec9766606bc86a4542ee948a1
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 22:03:59 +0300

    Update-ee828057-ea8b-49ab-9647-7e33b6bff362

--- src/main/java/com/bss/inc/redsmokes/api/config/annotations/DeleteIfIncomplete.java
+++ src/main/java/com/bss/inc/redsmokes/api/config/annotations/DeleteIfIncomplete.java
@@ -1,4 +1,10 @@
 package com.bss.inc.redsmokes.api.config.annotations;
 
-public @interface DeleteIfIncomplete {
-}
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DeleteIfIncomplete {}
