91283fdce431fdbdcdbca63d912921e5131f819d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 04:02:17 +0300

    Create RedSmokes.java

---
+++ src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -0,0 +1,22 @@
+package com.bss.inc.redsmokes.main;
+
+import com.bss.inc.redsmokes.api.IRedSmokes;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RedSmokes extends JavaPlugin implements IRedSmokes {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+}
