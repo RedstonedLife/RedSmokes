45e337dea48737fecaf2a25fe804026ecfa8f25c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 06:37:49 +0300

    Update-49b18b7c-fd4b-4c70-a6f4-74f4cf7eca97

---
+++ src/main/java/com/bss/inc/redsmokes/main/utils/logging/LoggerProvider.java
@@ -0,0 +1,12 @@
+package com.bss.inc.redsmokes.main.utils.logging;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginLogger;
+
+public abstract class LoggerProvider extends PluginLogger {
+    public LoggerProvider(final Plugin plugin) {
+        super(plugin);
+    }
+
+    protected
+}
