25261a0f9c1c27cfbeaeb9653549d6af3f97f235
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 21:55:30 +0300

    Update-725d4e9f-80a8-4d15-b039-9ff1eeab80c4

--- src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
+++ src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
@@ -2,7 +2,9 @@ package com.bss.inc.redsmokes.api;
 
 import com.bss.inc.redsmokes.api.commands.IrsCommand;
 
+import java.util.Map;
+
 public interface IRedSmokes {
     void reload();
-    Map<String, IrsCommand>
+    Map<String, IrsCommand> getCommandMap();
 }
