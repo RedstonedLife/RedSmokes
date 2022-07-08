4b09f3d15ce4192cc2603378496d7ff238689d3d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 06:40:36 +0300

    Update-3ac5d52b-d648-45a7-a3ad-d065a8739540

--- src/main/java/com/bss/inc/redsmokes/main/utils/logging/BaseLoggerProvider.java
+++ src/main/java/com/bss/inc/redsmokes/main/utils/logging/BaseLoggerProvider.java
@@ -1,5 +1,11 @@
 package com.bss.inc.redsmokes.main.utils.logging;
 
+import java.util.logging.Logger;
+
 public class BaseLoggerProvider extends LoggerProvider {
     private final Logger logger;
+
+    public BaseLoggerProvider(final Plugin plugin, final Logger logger) {
+
+    }
 }
