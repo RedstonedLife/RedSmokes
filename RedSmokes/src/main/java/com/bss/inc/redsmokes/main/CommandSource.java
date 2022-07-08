4b4a434a7987805b40613512800fec8dc5f8ca67
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 02:56:35 +0300

    Update-6e7d24f7-d9d6-4c66-8e7b-e9dc463d1c7d

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/CommandSource.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/CommandSource.java
@@ -1,4 +1,11 @@
 package com.bss.inc.redsmokes.main;
 
+import org.bukkit.command.CommandSender;
+
 public class CommandSource {
+    protected CommandSender sender;
+
+    public CommandSource(final CommandSender base) {
+        
+    }
 }
