2b615fbf4c1709f3961d12a451f52622891cabd3
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 02:48:53 +0300

    Update-88f5a370-9e84-431a-8c90-ff83ec8f6050

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/api/IUser.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/api/IUser.java
@@ -1,5 +1,8 @@
 package com.bss.inc.redsmokes.main.api;
 
+import com.bss.inc.redsmokes.api.commands.IrsCommand;
+
 public interface IUser {
-    
+    boolean isAuthorized(String node);
+    boolean isAuthorized(IrsCommand cmd)
 }
