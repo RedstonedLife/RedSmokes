13a552bbb343dd69fe6abd74a1f53875738d40fa
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 23:14:06 +0300

    Update

---
+++ providers/1_12Provider/src/main/java/com/bss/inc/redsmokes/FakeAccessor.java
@@ -0,0 +1,12 @@
+package com.bss.inc.redsmokes;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * Utility method to bridge certain features between our test suite and the base module
+ */
+public interface FakeAccessor {
+    void onPlayerJoin(PlayerJoinEvent event);
+    void getUser(Player player);
+}
