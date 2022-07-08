6fbcbe59c8419bf2b367cdc51bb8c0c7055814d5
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 07:03:26 +0300

    Update-9d12f3d4-babe-4b68-a869-cd1b12c7528d

--- src/main/java/com/bss/inc/redsmokes/main/utils/nms/refl/ReflUtil.java
+++ src/main/java/com/bss/inc/redsmokes/main/utils/nms/refl/ReflUtil.java
@@ -1,10 +1,12 @@
 package com.bss.inc.redsmokes.main.utils.nms.refl;
 
+import java.util.regex.Pattern;
+
 public final class ReflUtil {
     public static final NMSVersion
 
     public static final class NMSVersion implements Comparable<NMSVersion> {
-        private static final Pattern VERSION_PATTENR;
+        private static final Pattern VERSION_PATTENR = Pattern.compile("^v(\\d+)_(\\d+)_R(\\d+)")
     }
 }
 
