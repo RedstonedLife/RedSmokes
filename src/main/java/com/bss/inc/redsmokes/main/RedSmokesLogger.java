ca375297f64413ed0e6a928c0d7aacf21ba4cf31
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 07:33:10 +0300

    Update-9d26264e-6e14-4016-b802-c80d0fb7cba5

--- src/main/java/com/bss/inc/redsmokes/main/RedSmokesLogger.java
+++ src/main/java/com/bss/inc/redsmokes/main/RedSmokesLogger.java
@@ -1,5 +1,7 @@
 package com.bss.inc.redsmokes.main;
 
+import com.bss.inc.redsmokes.main.utils.logging.LoggerProvider;
+
 public class RedSmokesLogger {
-    
+    private final static Map<String, LoggerProvider>
 }
