56e218cd335c550232a87fe4b4242700d4708452
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 21:51:48 +0300

    Update-23a9f8a2-644c-48a2-b0eb-d2d17b7e559b

--- src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
+++ src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
@@ -3,8 +3,6 @@ package com.bss.inc.redsmokes.api;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public interface IRedSmokes {
-
-
-    public <T> RegisteredServiceProvider<T> getServiceProvider(Class<T> clazz);
+    
 
 }
