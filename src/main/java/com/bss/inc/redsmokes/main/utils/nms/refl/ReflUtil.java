fc33449ba9b3edbe455b78939b3e4fc1b3bdd8f8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 07:03:11 +0300

    Update-2fc02a82-76b6-4a9c-9477-ce5a06f2f974

--- src/main/java/com/bss/inc/redsmokes/main/utils/nms/refl/ReflUtil.java
+++ src/main/java/com/bss/inc/redsmokes/main/utils/nms/refl/ReflUtil.java
@@ -4,7 +4,7 @@ public final class ReflUtil {
     public static final NMSVersion
 
     public static final class NMSVersion implements Comparable<NMSVersion> {
-        
+        private static final Pattern VERSION_PATTENR;
     }
 }
 
