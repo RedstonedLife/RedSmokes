f8b38865af3fbc3c90e4c3223e5a0e3cc341523e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 06:32:25 +0300

    Update-11f631c6-7e4c-424a-b852-d82c895456f4

--- src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
+++ src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 public class RedSmokes extends JavaPlugin implements IRedSmokes {
 
-    final static Logger logger;
+    final static Logger logger
 
     @Override
     public void onEnable() {
