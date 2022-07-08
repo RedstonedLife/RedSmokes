0b35b80fda106707b87f65d9eb6060fce9d14c8a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 23:31:36 +0300

    Update-c57ba90d-0e63-40f7-890f-a70422e665cd

--- build-logic/src/main/kotlin/redsmokes.base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/redsmokes.base-conventions.gradle.kts
@@ -25,3 +25,61 @@ dependencies {
         }
     }
 }
+
+afterEvaluate {
+    if (baseExtension.injectBukkitApi.get()) {
+        dependencies {
+            api("org.spigotmc", "spigot-api", spigotVersion)
+        }
+    }
+    if (baseExtension.injectBstats.get()) {
+        dependencies {
+            implementation("org.bstats", "bstats-bukkit", "1.8")
+        }
+    }
+}
+
+tasks {
+    // Version Injection
+    processResources {
+        // Always process resources if version string or git branch changes
+        val fullVersion = rootProject.ext["FULL_VERSION"] as String
+        val gitBranch = rootProject.ext["GIT_BRANCH"] as String
+        inputs.property("fullVersion", fullVersion)
+        inputs.property("gitBranch", gitBranch)
+        filter<ReplaceTokens>(
+                "beginToken" to "\${",
+                "endToken" to "}",
+                "tokens" to mapOf(
+                        "full.version" to fullVersion,
+                        "git.branch" to gitBranch
+                )
+        )
+    }
+    compileJava {
+        options.compilerArgs.add("-Xlint:-deprecation")
+    }
+    javadoc {
+        title = "${project.name} API (v${rootProject.ext["FULL_VERSION"]})"
+        val options = options as? StandardJavadocDocletOptions ?: return@javadoc
+        options.links(
+                "https://hub.spigotmc.org/javadocs/spigot/"
+        )
+        options.addBooleanOption("Xdoclint:none", true)
+    }
+    withType<Jar> {
+        archiveVersion.set(rootProject.ext["FULL_VERSION"] as String)
+    }
+    withType<Sign> {
+        onlyIf { project.hasProperty("forceSign") }
+    }
+}
+
+// Dependency caching
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(5, "minutes")
+}
+
+
+// undo https://github.com/KyoriPowered/indra/blob/master/indra-common/src/main/kotlin/net/kyori/indra/IndraPlugin.kt#L57
+extensions.getByType<BasePluginExtension>().archivesName.set(project.name)
