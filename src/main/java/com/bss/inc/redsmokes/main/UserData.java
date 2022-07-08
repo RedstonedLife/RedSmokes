86ee3f815e4ed3438b812fa170a624e641f0774f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 22:01:53 +0300

    Update-7f980a09-e109-440a-abff-4b7c87b62272

--- src/main/java/com/bss/inc/redsmokes/main/UserData.java
+++ src/main/java/com/bss/inc/redsmokes/main/UserData.java
@@ -4,5 +4,6 @@ import com.bss.inc.redsmokes.api.IConf;
 import com.bss.inc.redsmokes.api.IRedSmokes;
 
 public abstract class UserData extends PlayerExtension implements IConf {
-    protected final transient IRedSmokes
+    protected final transient IRedSmokes redsmokes;
+    private final 
 }
