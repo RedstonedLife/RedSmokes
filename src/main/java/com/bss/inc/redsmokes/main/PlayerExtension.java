db9b2fb0a148e83f79fecdfdeba2e2b00cf01ce2
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 21:58:19 +0300

    Update-50d05671-69eb-4e43-b5db-97d7bf8d30ac

--- src/main/java/com/bss/inc/redsmokes/main/PlayerExtension.java
+++ src/main/java/com/bss/inc/redsmokes/main/PlayerExtension.java
@@ -1,4 +1,11 @@
 package com.bss.inc.redsmokes.main;
 
+import org.bukkit.entity.Player;
+
 public class PlayerExtension {
+    protected Player base;
+
+    public PlayerExtension(final Player base) {
+        this.base = base;
+    }
 }
