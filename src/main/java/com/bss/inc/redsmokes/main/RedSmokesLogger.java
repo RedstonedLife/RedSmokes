856368c68b1dce10bec619e6ff6968f7bc2c22ae
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 07:33:55 +0300

    Update-8fb75d35-48f8-49bf-bb89-698d0f9126da

--- src/main/java/com/bss/inc/redsmokes/main/RedSmokesLogger.java
+++ src/main/java/com/bss/inc/redsmokes/main/RedSmokesLogger.java
@@ -1,6 +1,8 @@
 package com.bss.inc.redsmokes.main;
 
 import com.bss.inc.redsmokes.main.utils.logging.LoggerProvider;
+import com.bss.inc.redsmokes.main.utils.nms.refl.ReflUtil;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.invoke.MethodHandle;
 import java.util.HashMap;
@@ -9,4 +11,11 @@ import java.util.Map;
 public class RedSmokesLogger {
     private final static Map<String, LoggerProvider> loggerProviders = new HashMap<>();
     private final static MethodHandle loggerFieldHandle;
+
+    static {
+        try {
+            final Field loggerField = ReflUtil.getFieldCached(JavaPlugin.class, "logger");
+            
+        }
+    }
 }
