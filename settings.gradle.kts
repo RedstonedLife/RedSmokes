3c0b4d514e6bd61e9264d254f95480733af973c0
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 23:49:52 +0300

    Update settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,47 @@
+dependencyResolutionManagement {
+    repositories {
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://jitpack.io") {
+            content { includeGroup("com.github.milkbowl") }
+        }
+        maven("https://repo.codemc.org/repository/maven-public") {
+            content { includeGroup("org.bstats") }
+        }
+        maven("https://m2.dv8tion.net/releases/") {
+            content { includeGroup("net.dv8tion") }
+        }
+        maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") {
+            content { includeGroup("me.clip") }
+        }
+        maven("https://libraries.minecraft.net/") {
+            content { includeGroup("com.mojang") }
+        }
+        mavenCentral {
+            content { includeGroup("net.kyori") }
+            content { includeGroup("org.apache.logging.log4j") }
+        }
+    }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+
+pluginManagement {
+    includeBuild("build-logic")
+}
+
+rootProject.name = "RedSmokesParent"
+
+// Modules
+sequenceOf(
+        "",
+).forEach {
+    include(":RedSmokes$it")
+    project(":RedSmokes$it").projectDir = file("RedSmokes$it")
+}
+
+// Providers
+include(":providers:BaseProviders")
+include(":providers:NMSReflectionProvider")
+include(":providers:PaperProvider")
+include(":providers:1_8Provider")
+include(":providers:1_12Provider")
