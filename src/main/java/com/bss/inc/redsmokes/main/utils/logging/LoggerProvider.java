f5233e4ff1f867fde7108e1066ba135c7c954b6c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-08 06:39:57 +0300

    Update-e287989e-6d43-41fe-a281-7d43fa8785e7

--- src/main/java/com/bss/inc/redsmokes/main/utils/logging/LoggerProvider.java
+++ src/main/java/com/bss/inc/redsmokes/main/utils/logging/LoggerProvider.java
@@ -14,33 +14,10 @@ public abstract class LoggerProvider extends PluginLogger {
     protected abstract void doTheLog(Level level, String message, Throwable throwable);
     protected abstract void doTheLog(Level level, String message);
 
-    @Override
-    public void log(Level level, String msg) {
-        doTheLog(level, msg);
-    }
-
-    @Override
-    public void log(Level level, String msg, Throwable thrown) {
-        doTheLog(level, msg, thrown);
-    }
-
-    @Override
-    public void log(LogRecord logRecord) {
-        doTheLog(logRecord.getLevel(), logRecord.getMessage(), logRecord.getThrown());
-    }
-
-    @Override
-    public void warning(String message) {
-        doTheLog(Level.WARNING, message);
-    }
-
-    @Override
-    public void info(String message) {
-        doTheLog(Level.INFO, message);
-    }
-
-    @Override
-    public void severe(String message) {
-        doTheLog(Level.SEVERE, message);
-    }
+    @Override public void log(Level level, String msg) {doTheLog(level, msg);}
+    @Override public void log(Level level, String msg, Throwable thrown) {doTheLog(level, msg, thrown);}
+    @Override public void log(LogRecord logRecord) {doTheLog(logRecord.getLevel(), logRecord.getMessage(), logRecord.getThrown());}
+    @Override public void warning(String message) {doTheLog(Level.WARNING, message);}
+    @Override public void info(String message) {doTheLog(Level.INFO, message);}
+    @Override public void severe(String message) {doTheLog(Level.SEVERE, message);}
 }
