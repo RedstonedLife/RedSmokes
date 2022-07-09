6bdbeee87ceee6bc4f9b53a90fd4a56e683fd402
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:06:25 +0300

    Update-49987958-fa51-4697-913b-9d1986ae8f49

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
@@ -3,6 +3,7 @@ package com.bss.inc.redsmokes.main;
 import com.bss.inc.redsmokes.api.IRedSmokes;
 import org.bukkit.Server;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Backup implements Runnable {
@@ -12,5 +13,5 @@ public class Backup implements Runnable {
     private transient boolean running = false;
     private transient int taskId = -1;
     private transient boolean active = false;
-    
+    private transient CompletableFuture<Object> taskLock = null;
 }
