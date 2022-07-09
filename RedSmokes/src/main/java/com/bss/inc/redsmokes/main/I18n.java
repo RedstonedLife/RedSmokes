1bdee274db63de7e3eb3cdf18870380e762d4bd0
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 19:46:32 +0300

    Update-2375a1d0-aee3-4d4f-8f0a-655be5abb67c

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
@@ -1,6 +1,9 @@
 package com.bss.inc.redsmokes.main;
 
+import java.util.regex.Pattern;
+
 public class I18n implements com.bss.inc.redsmokes.api.II18n {
     private static final String MESSAGES = "messages";
-    private static final Pattern 
+    private static final Pattern NODOUBLEMARK = Pattern.compile("''");
+    
 }
