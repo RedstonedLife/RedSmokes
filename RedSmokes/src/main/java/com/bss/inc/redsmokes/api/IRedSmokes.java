a239f0e2531244a0e06719ae1cdcfc634bf345ae
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 20:04:05 +0300

    Update-1ffcab01-c6dc-4bde-bc64-2c0ee65a5155

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
@@ -1,12 +1,18 @@
 package com.bss.inc.redsmokes.api;
 
 import com.bss.inc.redsmokes.api.commands.IrsCommand;
+import com.bss.inc.redsmokes.main.IRedSmokesModule;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
+import java.util.List;
 import java.util.Map;
 
 public interface IRedSmokes extends Plugin {
     void reload();
     Map<String, IrsCommand> getCommandMap();
+
+    List<String> onTabCompleteEssentials(CommandSender sender, Command command, String commandLabel, String[] args, ClassLoader classLoader, String commandPath, String permissionPrefix, IRedSmokesModule module);
 
 }
