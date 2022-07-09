90d67af4a60a738add340338b249ff49bf036f4a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 01:15:58 +0300

    Update-28c2eed8-cba8-4c09-99f9-c5bb53f1812b

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
@@ -266,7 +266,7 @@ public class UpdateChecker {
         private final String tag_name;
 
         RemoteVersion(BranchStatus branchStatus) {
-            this(branchStatus, 0);
+            this(branchStatus, "" 0);
         }
 
         RemoteVersion(BranchStatus branchStatus, String tag_name, int distance) {
