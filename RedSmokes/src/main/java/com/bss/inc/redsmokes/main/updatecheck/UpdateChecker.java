227530ba51b3a83ebb3c4959c6870df2cad542bb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:55:32 +0300

    Update-f153181c-a387-47cb-9cc3-307d4e4ca4af

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.List;
