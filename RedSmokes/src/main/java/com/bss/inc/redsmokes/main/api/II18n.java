c9a2605d5ff78f71e441e16f986054a011c7e2d4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 19:44:22 +0300

    Update-a0e61c48-5130-4f02-94d3-0b031dc1b6a2

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/api/II18n.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/api/II18n.java
@@ -1,4 +1,10 @@
 package com.bss.inc.redsmokes.main.api;
 
+/**
+ * Provides access to the current locale in use.
+ *
+ * @deprecated External plugins should prefer to use either the player's client language ({@link Player#getLocale()} or
+ *             {@link com.} in case of future additions.
+ */
 public interface II18n {
 }
