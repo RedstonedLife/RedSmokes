5090eea7d9466b844987da69925751cbd2193885
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 19:46:47 +0300

    Update-c0c64a3d-a5a2-48e0-b84d-28d788a9df32

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
@@ -1,10 +1,21 @@
 package com.bss.inc.redsmokes.main;
 
+import java.util.Enumeration;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
 public class I18n implements com.bss.inc.redsmokes.api.II18n {
     private static final String MESSAGES = "messages";
     private static final Pattern NODOUBLEMARK = Pattern.compile("''");
-    private static final ResourceBundle
+    private static final ResourceBundle NULL_BUNDLE = new ResourceBundle() {
+        @Override
+        protected Object handleGetObject(String key) {
+            return null;
+        }
+
+        @Override
+        public Enumeration<String> getKeys() {
+            return null;
+        }
+    }
 }
