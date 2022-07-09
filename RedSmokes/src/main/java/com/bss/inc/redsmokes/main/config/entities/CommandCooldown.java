820fb54c8a4d7ee694fe295f0f655c8a978a360a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 03:37:19 +0300

    Update-edbaf925-3c02-4d53-bdfc-451482d24f9f

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/config/entities/CommandCooldown.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/config/entities/CommandCooldown.java
@@ -1,4 +1,32 @@
 package com.bss.inc.redsmokes.main.config.entities;
 
-public class CommandCooldown {
+import com.bss.inc.redsmokes.main.config.processors.DeleteIfIncompleteProcessor;
+
+import java.util.regex.Pattern;
+
+public class CommandCooldown implements DeleteIfIncompleteProcessor.IncompleteEntity {
+    private Pattern pattern;
+
+    public Pattern pattern() {
+        return this.pattern;
+    }
+
+    public void pattern(final Pattern value) {
+        this.pattern = value;
+    }
+
+    private Long value;
+
+    public Long value() {
+        return this.value;
+    }
+
+    public void value(final Long value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean isIncomplete() {
+        return value == null || pattern == null;
+    }
 }
