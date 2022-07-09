4b4d59fcfc9e1637fe3620f33df6c74ac792d5cc
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 23:34:17 +0300

    Update-fd19be0e-3be8-4ae5-93b8-8887b58a97cc

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/IText.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/IText.java
@@ -6,5 +6,5 @@ public interface IText {
     // Contains the raw text lines
     List<String> getLines();
 
-    // Chapters contain the names  that are displayed
+    // Chapters contain the names  that are displayed automatically
 }
