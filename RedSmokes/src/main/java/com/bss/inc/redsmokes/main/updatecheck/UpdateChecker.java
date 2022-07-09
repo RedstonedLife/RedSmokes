0a38aa3296ed3c067b6dc45fc97fe22d1476617e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:52:43 +0300

    Update-bbe67aa3-a6ef-469c-93bc-6bd2384c18d1

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
@@ -20,5 +20,5 @@ public class UpdateChecker {
     private long lastFetchTime = 0;
     private CompletableFuture<RemoteVersion> pendingDevFuture;
     private CompletableFuture<RemoteVersion> pendingReleaseFuture;
-    private String 
+    private String latestRelease = 
 }
