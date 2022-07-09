19853694670f1dd8ad2df0c8a2cc6c00d3076356
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:56:21 +0300

    Update-dda6567f-a94c-4119-b49d-ef7879aa24ce

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
@@ -12,7 +12,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.text.MessageFormat;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
@@ -140,7 +139,7 @@ public class UpdateChecker {
         return latestRelease;
     }
 
-    private HttpURLConnection tryRequestWithFallback(final String githubUrl, final String fallbackUrl) throws IOException {
+    private HttpURLConnection tryRequestWithFallback(final String githubUrl) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) new URL(githubUrl).openConnection();
         try {
             connection.connect();
