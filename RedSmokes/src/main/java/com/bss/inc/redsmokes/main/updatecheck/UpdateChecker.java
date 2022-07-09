b075ecbc2a798d74d9e46616e3936c520a786bc4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:48:14 +0300

    Update-d66793fa-6033-4762-b878-b50cb908eb1e

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
@@ -1,5 +1,7 @@
 package com.bss.inc.redsmokes.main.updatecheck;
 
+import com.bss.inc.redsmokes.main.RedSmokes;
+
 public class UpdateChecker {
     private static final String REPO = "RedSmokes/RedSmokes";
     private static final String BRANCH = "main";
@@ -7,4 +9,6 @@ public class UpdateChecker {
     private static final String LATEST_RELEASE_URL = "https://api.github.com/repos/"+REPO+"/releases/latest";
     // 0 = base for comparison, 1 = head for comparison - *not* the same as what this class calls them
     private static final String DISTANCE_URL = "https://api.github.com/repos/RedSmokes/RedSmokes/compare/{0}...{1}";
+
+    private final RedSmokes
 }
