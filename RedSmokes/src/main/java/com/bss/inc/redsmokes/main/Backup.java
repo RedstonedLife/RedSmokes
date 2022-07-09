f26249d5033494703ffc1fa1ceac9e8e30a4d871
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:05:35 +0300

    Update-774f329e-ecad-49b4-a0b5-31455ab7fe3e

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
@@ -1,8 +1,9 @@
 package com.bss.inc.redsmokes.main;
 
+import com.bss.inc.redsmokes.api.IRedSmokes;
 import org.bukkit.Server;
 
 public class Backup implements Runnable {
     private transient final Server server;
-    private transient final 
+    private transient final IRedSmokes
 }
