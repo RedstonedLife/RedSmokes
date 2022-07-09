95b486d0a94752329daba63f2d334cb2380e9808
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 23:39:56 +0300

    Update-0f2227ba-b902-4a26-9b8c-7ba063277f3c

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/BookInput.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/BookInput.java
@@ -19,7 +19,7 @@ public class BookInput implements IText {
     public BookInput(final String filename, final boolean createFile, final IRedSmokes redSmokes) throws IOException {
         File file = null;
         if(file == null || !file.exists()) {
-            
+            file = new File(redSmokes.getDataFolder())
         }
     }
 }
