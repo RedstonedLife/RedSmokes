9c1682bc23b6585547596be8613f226a98ba4597
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 21:58:36 +0300

    Update-41174c33-b798-4cdd-be3a-a0ecaa6044bd

---
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
@@ -0,0 +1,48 @@
+package com.bss.inc.redsmokes.api;
+
+import com.bss.inc.redsmokes.api.commands.IrsCommand;
+import com.bss.inc.redsmokes.main.*;
+import com.bss.inc.redsmokes.main.commands.PlayerNotFoundException;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+public interface IRedSmokes extends Plugin {
+    void reload();
+    Map<String, IrsCommand> getCommandMap();
+    List<String> onTabCompleteRedSmokes(CommandSender sender, Command command, String commandLabel, String[] args, ClassLoader classLoader, String commandPath, String permissionPrefix, IRedSmokesModule module);
+    boolean onCommandEssentials(CommandSender sender, Command command, String commandLabel, String[] args, ClassLoader classLoader, String commandPath, String permissionPrefix, IRedSmokesModule module);
+    @Deprecated
+    User getUser(Object base);
+    User getUser(UUID base);
+    User getUser(String base);
+    User getUser(Player base);
+    User matchUser(Server server, User sourceUser, String searchTerm, Boolean getHidden, boolean getOffline) throws PlayerNotFoundException;
+    boolean canInteractWith(CommandSource interactor, User interactee);
+    boolean canInteractWith(User interactor, User interactee);
+    I18n getI18n();
+    User getOfflineUser(String name);
+    World getWorld(String name);
+    int broadcastMessage(String message);
+    int broadcastMessage(IUser sender, String message);
+    int broadcastMessage(IUser sender, String message, Predicate<IUser> shouldExclude);
+    int broadcastMessage(String permission, String message);
+    ISettings getSettings();
+    BukkitScheduler getScheduler();
+    Backup getBackup();
+    UpdateChecker getUpdateChecker();
+    BukkitTask runTaskAsynchronously(Runnable run);
+    BukkitTask runTaskLaterAsynchronously(Runnable run, long delay);
+    BukkitTask runTaskTimerAsynchronously(Runnable run, long delay, long period);
+}
