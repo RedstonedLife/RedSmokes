39ecdc56e0cb42c89e9d2e09f8a0a308a150cdcd
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 19:44:50 +0300

    Update-4d16058e-d6ec-4760-b57e-c7745fcdb235

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/api/II18n.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/api/II18n.java
@@ -9,5 +9,10 @@ import org.bukkit.entity.Player;
  *             {@link com.bss.inc.redsmokes.api.II18n } in case of future additions.
  */
 public interface II18n {
-    
+    /**
+     * Gets the current locale setting
+     *
+     * @return the current locale, if not set it will return the default locale
+     */
+    Locale getCurrentLocale()
 }
