65374784a4f95a44f1c88d8d812abd58492e7b1d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 03:41:11 +0300

    Update-7c45624f-c331-447d-9cb8-17e5840d4da1

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/IUser.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/IUser.java
@@ -66,5 +66,5 @@ public interface IUser {
     boolean isPromptingPayConfirm();
     void setPromptingPayConfirm(boolean prompt);
     boolean isPromptingClearConfirm();
-    void
+    void setPromptingClearConfirm()
 }
