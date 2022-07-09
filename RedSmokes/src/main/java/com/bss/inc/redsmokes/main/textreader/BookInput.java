43a3a1b8c957bec33ea0933953000f228f660e8f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 23:39:41 +0300

    Update-20062fce-9c39-4e89-8102-317ffc7ff92e

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/BookInput.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/BookInput.java
@@ -2,6 +2,7 @@ package com.bss.inc.redsmokes.main.textreader;
 
 import com.bss.inc.redsmokes.api.IRedSmokes;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
@@ -16,6 +17,7 @@ public class BookInput implements IText {
     private final transient long lastChange;
 
     public BookInput(final String filename, final boolean createFile, final IRedSmokes redSmokes) throws IOException {
+        File file = null;
         
     }
 }
