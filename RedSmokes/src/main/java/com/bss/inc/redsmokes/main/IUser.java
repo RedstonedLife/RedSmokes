7242522a85dd2e304bf8955410ab7b005eef881a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 03:19:17 +0300

    Update-79da1692-85dd-461f-958e-4457130666f7

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/IUser.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/IUser.java
@@ -34,5 +34,5 @@ public interface IUser {
     String getGroup();
     boolean inGroup(final String group);
     void sendMessage(String message);
-    void sendMail(MailSender, String message);
+    void sendMail(MailSender sender, String message);
 }
