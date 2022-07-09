11bc2804010ffecbffe1f2afd115806b594ba0f8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:05:49 +0300

    Update-0ca4e660-cb12-4598-95dc-70e1fb68bbe2

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
@@ -3,8 +3,10 @@ package com.bss.inc.redsmokes.main;
 import com.bss.inc.redsmokes.api.IRedSmokes;
 import org.bukkit.Server;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Backup implements Runnable {
     private transient final Server server;
     private transient final IRedSmokes redSmokes;
-    
+    private final AtomicBoolean pendingShutdown = new AtomicBoolean(false);
 }
