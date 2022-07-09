f2d4f19f7339ddd8cd651829b71c2130ffc92cdb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:39:54 +0300

    Update-21181d79-c277-491e-bdc8-6a14460d99bc

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
@@ -2,4 +2,5 @@ package com.bss.inc.redsmokes.main.updatecheck;
 
 public class UpdateChecker {
     private static final String REPO = "RedSmokes/RedSmokes";
+    private static final String BRANCH = "main";
 }
