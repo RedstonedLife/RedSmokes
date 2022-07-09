ebb4cf9a2974b909647e767a9a092b544d56e44e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 20:09:49 +0300

    Update-714023d1-51ad-40e3-ad29-9b87b42eeac3

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
@@ -2,10 +2,12 @@ package com.bss.inc.redsmokes.api;
 
 import com.bss.inc.redsmokes.api.commands.IrsCommand;
 import com.bss.inc.redsmokes.main.CommandSource;
+import com.bss.inc.redsmokes.main.I18n;
 import com.bss.inc.redsmokes.main.IRedSmokesModule;
 import com.bss.inc.redsmokes.main.User;
 import com.bss.inc.redsmokes.main.commands.PlayerNotFoundException;
 import org.bukkit.Server;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,6 +16,7 @@ import org.bukkit.plugin.Plugin;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Predicate;
 
 public interface IRedSmokes extends Plugin {
     void reload();
@@ -28,4 +31,11 @@ public interface IRedSmokes extends Plugin {
     User matchUser(Server server, User sourceUser, String searchTerm, Boolean getHidden, boolean getOffline) throws PlayerNotFoundException;
     boolean canInteractWith(CommandSource interactor, User interactee);
     boolean canInteractWith(User interactor, User interactee);
+    I18n getI18n();
+    User getOfflineUser(String name);
+    World getWorld(String name);
+    int broadcastMessage(String message);
+    int broadcastMessage(IUser sender, String message);
+    int broadcastMessage(IUser sender, String message, Predicate<IUser> shouldExclude);
+    int broadcastMessage(String permission, String message);
 }
