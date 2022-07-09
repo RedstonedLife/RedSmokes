2085ce2daf164a8e836c10d335a4cf4b8e0dc7f6
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 22:22:55 +0300

    Update-88d8cd2b-ecd7-4c69-aeef-b2c480460b4b

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Trade.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Trade.java
@@ -1,5 +1,8 @@
 package com.bss.inc.redsmokes.main;
 
+import com.bss.inc.redsmokes.api.IRedSmokes;
+import org.bukkit.inventory.ItemStack;
+
 import java.io.FileWriter;
 import java.math.BigDecimal;
 
@@ -8,5 +11,7 @@ public class Trade {
     private final transient String command;
     private final transient Trade fallbackTrade;
     private final transient BigDecimal money;
-    private final transient 
+    private final transient ItemStack itemStack;
+    private final transient Integer exp;
+    private final transient IRedSmokes redSmokes;
 }
