ffa4eb5bfa8d15ffa560df89174bd791594cddaa
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 23:38:47 +0300

    Update-54778caa-ec70-4edd-8c60-3e609b8236cd

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/BookInput.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/BookInput.java
@@ -3,10 +3,12 @@ package com.bss.inc.redsmokes.main.textreader;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BookInput implements IText {
     private final static HashMap<String, SoftReference<BookInput>> cache = new HashMap<>();
     private final transient List<String> lines;
     private final transient List<String> chapters;
-    private final transient Map<String, Integer>
+    private final transient Map<String, Integer> bookmarks;
+    private final transient 
 }
