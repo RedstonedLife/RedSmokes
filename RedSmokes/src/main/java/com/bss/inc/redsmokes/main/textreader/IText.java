1e57f9dc84f56b95f90424aff95b54cd3797f76d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 23:34:51 +0300

    Update-ab9a9e08-6c1a-42cd-9373-7c640f7f7275

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/IText.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/textreader/IText.java
@@ -9,5 +9,5 @@ public interface IText {
     // Chapters contain the names that are displayed automatically if the file doesn't contain a introduction chapter.
     List<String> getChapters();
 
-    // Bookmarks
+    // Bookmarks contains the strings
 }
