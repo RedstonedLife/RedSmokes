218dd293581f43414e4e4f6a619760e376f0a57b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:08:52 +0300

    Update-c7b30740-cc95-4a73-b0d1-149273b1d2db

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Backup.java
@@ -42,6 +42,7 @@ public class Backup implements Runnable {
                 return;
             }
             taskId = redSmokes.scheduleSyncRepeatingTask(this, interval, interval);
+            running = true;
         }
     }
 }
