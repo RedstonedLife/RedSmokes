675ad02b93aa5dc85e51689907c3f4b377f6dafb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 19:46:17 +0300

    Update-e42ddce3-f2b6-42ed-a211-3db6255de05d

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
@@ -1,4 +1,6 @@
 package com.bss.inc.redsmokes.main;
 
 public class I18n implements com.bss.inc.redsmokes.api.II18n {
+    private static final String MESSAGES = "messages";
+    private static final Pattern 
 }
