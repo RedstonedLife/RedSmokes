e4e72525211df304169e551934d136b1a08b9a5c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 00:39:03 +0300

    Update-e55cba5a-4005-4c0a-b4bf-5f03ec23add4

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/updatecheck/UpdateChecker.java
@@ -1,5 +1,5 @@
 package com.bss.inc.redsmokes.main.updatecheck;
 
 public class UpdateChecker {
-    private static final String REPO = "RedSmokes/"
+    private static final String REPO = "RedSmokes/RedSmokes"
 }
