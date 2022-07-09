85458251a1e6c0d710dc327cfba81d7499b59137
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 19:52:04 +0300

    Update-a06f0415-8b06-49b2-9686-80f4f50807e0

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/I18n.java
@@ -31,6 +31,8 @@ public class I18n implements com.bss.inc.redsmokes.api.II18n {
 
     public static String tl(final String string, final Object... objects) {
         if(instance==null){return "";}
-        if(objects.length)
+        if(objects.length==0) {
+            return NODOUBLEMARK.matcher(instance.translate(string)).replaceAll("'");
+        }
     }
 }
