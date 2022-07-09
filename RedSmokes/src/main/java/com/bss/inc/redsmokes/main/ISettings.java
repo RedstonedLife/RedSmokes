0062081564ac489642d361e7e2c4d6572dd5b4f8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 20:13:15 +0300

    Update-da0a0478-5348-4cf0-91f4-f5ae014f9d76

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/ISettings.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/ISettings.java
@@ -1,12 +1,17 @@
 package com.bss.inc.redsmokes.main;
 
 import com.bss.inc.redsmokes.api.IConf;
+import com.bss.inc.redsmokes.api.commands.IrsCommand;
 
 import java.io.File;
+import java.math.BigDecimal;
 
 public interface ISettings extends IConf {
     File getConfigFile();
     String getBackupCommand();
     long getBackupInterval();
     boolean isAlwaysRunBackup();
+    BigDecimal getCommandCost(IrsCommand cmd);
+    BigDecimal getCommandCost(String label);
+    
 }
