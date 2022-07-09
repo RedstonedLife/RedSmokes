c7be9a1a99598fe9a07a8358f91d69c6473a9cd4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-10 02:11:57 +0300

    Update-5c72fae6-2b1c-481d-8148-3f48202a5afc

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/perm/IPermissionsHandler.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/perm/IPermissionsHandler.java
@@ -2,7 +2,9 @@ package com.bss.inc.redsmokes.main.perm;
 
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public interface IPermissionsHandler {
     String getGroup(Player base);
-    
+    List<String> getGroups()
 }
