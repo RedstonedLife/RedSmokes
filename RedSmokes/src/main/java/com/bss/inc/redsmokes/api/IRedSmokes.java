cfb876d61be7da67882db0b440590f93d262994b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 20:05:08 +0300

    Update-b16ae2cb-b2db-4be8-b96e-2d389def9575

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
@@ -2,17 +2,25 @@ package com.bss.inc.redsmokes.api;
 
 import com.bss.inc.redsmokes.api.commands.IrsCommand;
 import com.bss.inc.redsmokes.main.IRedSmokesModule;
+import com.bss.inc.redsmokes.main.User;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public interface IRedSmokes extends Plugin {
     void reload();
     Map<String, IrsCommand> getCommandMap();
     List<String> onTabCompleteRedSmokes(CommandSender sender, Command command, String commandLabel, String[] args, ClassLoader classLoader, String commandPath, String permissionPrefix, IRedSmokesModule module);
     boolean onCommandEssentials(CommandSender sender, Command command, String commandLabel, String[] args, ClassLoader classLoader, String commandPath, String permissionPrefix, IRedSmokesModule module);
-    @Deprecated User getUser(Object base);
+    @Deprecated
+    User getUser(Object base);
+    User getUser(UUID base);
+    User getUser(String base);
+    User getUser(Player base);
+    
 }
