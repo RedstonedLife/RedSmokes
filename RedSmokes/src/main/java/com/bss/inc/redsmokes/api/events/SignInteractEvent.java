cd44fcc12236f1cdfb440c7f36d1f16de3f3a140
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 22:11:15 +0300

    Update-fad9668c-d289-4d1b-bef5-ed15367b34f8

---
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/api/events/SignInteractEvent.java
@@ -0,0 +1,26 @@
+package com.bss.inc.redsmokes.api.events;
+
+import com.bss.inc.redsmokes.main.signs.RedSmokesSign;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Fired when an Essentials sign is interacted with.
+ *
+ * This is primarily intended for use with EssentialsX's sign abstraction - external plugins should not listen on this event.
+ */
+public class SignInteractEvent extends SignEvent {
+    private static final HandlerList handlers = new HandlerList();
+
+    public SignInteractEvent(final RedSmokesSign.ISign sign, final RedSmokesSign essSign, final IUser user) {
+        super(sign, essSign, user);
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
