8ff229292c2c7f55bfe0138317d2ecb2644d4aac
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-09 22:28:04 +0300

    Update-bcbb95a9-6c6a-4214-adfa-aed4eff42407

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Trade.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Trade.java
@@ -3,8 +3,13 @@ package com.bss.inc.redsmokes.main;
 import com.bss.inc.redsmokes.api.IRedSmokes;
 import org.bukkit.inventory.ItemStack;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.logging.Level;
 
 public class Trade {
     private static FileWriter fw = null;
@@ -44,5 +49,97 @@ public class Trade {
         this.redSmokes = redSmokes;
     }
 
-    
+    public static void log(final String type, final String subtype, final String event, final String sender, final Trade charge, final String receiver, final Trade pay, final Location loc, final BigDecimal endBalance, final IEssentials ess) {
+        //isEcoLogUpdateEnabled() - This refers to log entries with no location, ie API updates #EasterEgg
+        //isEcoLogEnabled() - This refers to log entries with with location, ie /pay /sell and eco signs.
+
+        if ((loc == null && !ess.getSettings().isEcoLogUpdateEnabled()) || (loc != null && !ess.getSettings().isEcoLogEnabled())) {
+            return;
+        }
+        if (fw == null) {
+            try {
+                fw = new FileWriter(new File(ess.getDataFolder(), "trade.log"), true);
+            } catch (final IOException ex) {
+                RedSmokes.getWrappedLogger().log(Level.SEVERE, null, ex);
+            }
+        }
+        final StringBuilder sb = new StringBuilder();
+        sb.append(type).append(",").append(subtype).append(",").append(event).append(",\"");
+        sb.append(DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL).format(new Date()));
+        sb.append("\",\"");
+        if (sender != null) {
+            sb.append(sender);
+        }
+        sb.append("\",");
+        if (charge == null) {
+            sb.append("\"\",\"\",\"\"");
+        } else {
+            if (charge.getItemStack() != null) {
+                sb.append(charge.getItemStack().getAmount()).append(",");
+                sb.append(charge.getItemStack().getType()).append(",");
+                if (VersionUtil.PRE_FLATTENING) {
+                    sb.append(charge.getItemStack().getDurability());
+                }
+            }
+            if (charge.getMoney() != null) {
+                sb.append(charge.getMoney()).append(",");
+                sb.append("money").append(",");
+                sb.append(ess.getSettings().getCurrencySymbol());
+            }
+            if (charge.getExperience() != null) {
+                sb.append(charge.getExperience()).append(",");
+                sb.append("exp").append(",");
+                sb.append("\"\"");
+            }
+        }
+        sb.append(",\"");
+        if (receiver != null) {
+            sb.append(receiver);
+        }
+        sb.append("\",");
+        if (pay == null) {
+            sb.append("\"\",\"\",\"\"");
+        } else {
+            if (pay.getItemStack() != null) {
+                sb.append(pay.getItemStack().getAmount()).append(",");
+                sb.append(pay.getItemStack().getType()).append(",");
+                if (VersionUtil.PRE_FLATTENING) {
+                    sb.append(pay.getItemStack().getDurability());
+                }
+            }
+            if (pay.getMoney() != null) {
+                sb.append(pay.getMoney()).append(",");
+                sb.append("money").append(",");
+                sb.append(ess.getSettings().getCurrencySymbol());
+            }
+            if (pay.getExperience() != null) {
+                sb.append(pay.getExperience()).append(",");
+                sb.append("exp").append(",");
+                sb.append("\"\"");
+            }
+        }
+        if (loc == null) {
+            sb.append(",\"\",\"\",\"\",\"\"");
+        } else {
+            sb.append(",\"");
+            sb.append(loc.getWorld().getName()).append("\",");
+            sb.append(loc.getBlockX()).append(",");
+            sb.append(loc.getBlockY()).append(",");
+            sb.append(loc.getBlockZ()).append(",");
+        }
+
+        if (endBalance == null) {
+            sb.append(",");
+        } else {
+            sb.append(endBalance);
+            sb.append(",");
+        }
+        sb.append("\n");
+        try {
+            fw.write(sb.toString());
+            fw.flush();
+        } catch (final IOException ex) {
+            RedSmokes.getWrappedLogger().log(Level.SEVERE, null, ex);
+        }
+    }
 }
