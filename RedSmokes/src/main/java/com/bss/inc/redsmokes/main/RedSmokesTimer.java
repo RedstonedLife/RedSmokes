8d47ce54b9dc8429ab1ddc61b68112b1f0449cb6
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 02:01:05 +0300

    Update-57abaaa4-91ef-44c8-a291-630881ab003a

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokesTimer.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokesTimer.java
@@ -1,11 +1,14 @@
 package com.bss.inc.redsmokes.main;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.UUID;
 
 public class RedSmokesTimer implements Runnable {
     private final transient IRedSmokes redSmokes;
     private final transient Set<UUID> onlineUsers = new HashSet<>();
+    private final LinkedList<Double> history = new LinkedList<>();
+    @SuppressWarnings("FieldCanBeLocal")
     
 }
