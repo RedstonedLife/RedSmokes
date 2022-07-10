a162acbd39edb90342535a4dc594fcd5cc2ced0e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 02:31:12 +0300

    Update-b20ef3b6-3bc1-4e9f-9ae6-4259e405e8e2

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
@@ -1,7 +1,8 @@
 package com.bss.inc.redsmokes.main;
 
+import com.bss.inc.redsmokes.main.utils.EnumUtil;
 import org.bukkit.Statistic;
 
 public class User implements com.bss.inc.redsmokes.api.IUser, Comparable<User> {
-    private static final Statistic
+    private static final Statistic PLAY_ONE_TICK = EnumUtil.getStatistic("PLAY_ONE_MINUTE", "PLAY_ONE_TICK");
 }
