634794425f9cd7647c9e4c126c50bdda096d95ba
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 02:33:45 +0300

    Update-7964cc56-32fb-4c55-a659-c5960e7ca4f9

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
@@ -20,4 +20,6 @@ public class User implements com.bss.inc.redsmokes.api.IUser, Comparable<User> {
     // Misc
     private transient long lastOnlineActivity;
     private transient long lastThrottledAction;
+    
+
 }
