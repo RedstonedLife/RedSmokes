1a0d9a6e40ef50aa0b723e94af4a42229d30e78f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 02:30:52 +0300

    Update-01a3761e-e990-4962-9967-30f08a9fadb6

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
@@ -1,4 +1,7 @@
 package com.bss.inc.redsmokes.main;
 
+import org.bukkit.Statistic;
+
 public class User implements com.bss.inc.redsmokes.api.IUser, Comparable<User> {
+    private static final Statistic
 }
