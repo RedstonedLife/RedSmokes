25fbc3d510d3419dcf677f240dfa4314e18a1945
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 02:31:43 +0300

    Update-b1ac00bb-40b1-430c-99ef-77e820e34930

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
@@ -3,9 +3,13 @@ package com.bss.inc.redsmokes.main;
 import com.bss.inc.redsmokes.main.utils.EnumUtil;
 import org.bukkit.Statistic;
 
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class User implements com.bss.inc.redsmokes.api.IUser, Comparable<User> {
     private static final Statistic PLAY_ONE_TICK = EnumUtil.getStatistic("PLAY_ONE_MINUTE", "PLAY_ONE_TICK");
 
     // User command confirmation strings
-    private final Map<>
+    private final Map<User, BigDecimal> confirmingPayments = new WeakHashMap<>();
 }
