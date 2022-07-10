3ee4cd0f10ddf2fd6a8c4fcb16f7597db5ef69bf
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 02:21:43 +0300

    Update-f6ac25bf-3451-469a-bb6d-34b8ec066a75

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokesTimer.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokesTimer.java
@@ -58,7 +58,7 @@ public class RedSmokesTimer implements Runnable {
             count++;
             if(skip2 > 0) {skip2--;continue;}
             if(count % 10 == 0) {if(System.nanoTime() - startTime > maxTime) {skip2 = count - 1;break;}}
-            
+            final User user = redSmokes.getUser(iterator.next());
         }
     }
 
