be1144e458ff3439b6b6fe71a3db37fada54f95c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 02:04:25 +0300

    Update-20187b48-7a40-40f1-adb8-7ab22f419972

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokesTimer.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokesTimer.java
@@ -30,6 +30,8 @@ public class RedSmokesTimer implements Runnable {
         if(history.size() > 10) {history.remove();}
         final double tps = tickInterval * 1000000.0 / timeSpent;
         if(tps <= 21) {history.add(tps);}
+        lastPoll = startTime;
+        
     }
 
 }
