fdfc6ba295487175be3409dbe46d4fee7653aaba
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 03:53:35 +0300

    Update-4b50ed94-9ac0-4d10-ba2c-bd87e0c6fef1

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserData.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserData.java
@@ -3,8 +3,13 @@ package com.bss.inc.redsmokes.main;
 import com.bss.inc.redsmokes.api.IConf;
 import com.bss.inc.redsmokes.api.IRedSmokes;
 import com.bss.inc.redsmokes.main.config.RedSmokesUserConfiguration;
+import com.bss.inc.redsmokes.main.config.holders.UserConfigHolder;
+
+import java.math.BigDecimal;
 
 public abstract class UserData extends PlayerExtension implements IConf {
     protected final transient IRedSmokes redsmokes;
     private final RedSmokesUserConfiguration config;
+    private UserConfigHolder holder;
+    private BigDecimal money;
 }
