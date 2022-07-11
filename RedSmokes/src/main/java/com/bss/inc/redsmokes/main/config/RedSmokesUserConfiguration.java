84bc79bd07770ae183b6b75336e41139b09ae0bf
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 03:45:34 +0300

    Update-5b7d7f7e-b2ef-46cc-b87f-f1f41ad17ddc

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/config/RedSmokesUserConfiguration.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/config/RedSmokesUserConfiguration.java
@@ -1,4 +1,74 @@
 package com.bss.inc.redsmokes.main.config;
 
-public class RedSmokesUserConfiguration {
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class EssentialsUserConfiguration extends RedSmokesConfiguration {
+    private String username;
+    private final UUID uuid;
+
+    public EssentialsUserConfiguration(final String username, final UUID uuid, final File configFile) {
+        super(configFile);
+        this.username = username;
+        this.uuid = uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean legacyFileExists() {
+        return new File(configFile.getParentFile(), username + ".yml").exists();
+    }
+
+    @Override
+    public void convertLegacyFile() {
+        final File file = new File(configFile.getParentFile(), username + ".yml");
+        try {
+            //noinspection UnstableApiUsage
+            Files.move(file, new File(configFile.getParentFile(), uuid + ".yml"));
+        } catch (final IOException ex) {
+            Essentials.getWrappedLogger().log(Level.WARNING, "Failed to migrate user: " + username, ex);
+        }
+
+        setProperty("last-account-name", username);
+    }
+
+    private File getAltFile() {
+        final UUID fn = UUID.nameUUIDFromBytes(("OfflinePlayer:" + username.toLowerCase(Locale.ENGLISH)).getBytes(Charsets.UTF_8));
+        return new File(configFile.getParentFile(), fn.toString() + ".yml");
+    }
+
+    @Override
+    public boolean altFileExists() {
+        if (username.equals(username.toLowerCase())) {
+            return false;
+        }
+        return getAltFile().exists();
+    }
+
+    @Override
+    public void convertAltFile() {
+        try {
+            //noinspection UnstableApiUsage
+            Files.move(getAltFile(), new File(configFile.getParentFile(), uuid + ".yml"));
+        } catch (final IOException ex) {
+            Essentials.getWrappedLogger().log(Level.WARNING, "Failed to migrate user: " + username, ex);
+        }
+    }
 }
