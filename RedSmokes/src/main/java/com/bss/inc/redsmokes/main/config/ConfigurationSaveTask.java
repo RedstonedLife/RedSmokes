9efcf4cae82332b099be217bd82f511fecd26183
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 03:37:06 +0300

    Update-9bab4be9-9f91-4a87-a76c-109c80f1b77e

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/config/ConfigurationSaveTask.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/config/ConfigurationSaveTask.java
@@ -1,4 +1,39 @@
 package com.bss.inc.redsmokes.main.config;
 
-public class ConfigurationSaveTask {
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+
+public class ConfigurationSaveTask implements Runnable {
+    private final YamlConfigurationLoader loader;
+    private final CommentedConfigurationNode node;
+    private final AtomicInteger pendingWrites;
+
+    public ConfigurationSaveTask(final YamlConfigurationLoader loader, final CommentedConfigurationNode node, final AtomicInteger pendingWrites) {
+        this.loader = loader;
+        this.node = node;
+        this.pendingWrites = pendingWrites;
+    }
+
+    @Override
+    public void run() {
+        synchronized (loader) {
+            // Check if there are more writes in queue.
+            // If that's the case, we shouldn't bother writing data which is already out-of-date.
+            if (pendingWrites.get() > 1) {
+                pendingWrites.decrementAndGet();
+            }
+
+            try {
+                loader.save(node);
+            } catch (ConfigurateException e) {
+                Essentials.getWrappedLogger().log(Level.SEVERE, e.getMessage(), e);
+            } finally {
+                pendingWrites.decrementAndGet();
+            }
+        }
+    }
 }
