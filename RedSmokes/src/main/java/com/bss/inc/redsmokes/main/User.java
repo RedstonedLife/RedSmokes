225ae742359d7894ae2b6b3110a20e88dbef6594
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 05:15:57 +0300

    Update-8a57e145-094f-42ae-bd03-0eeaaa2d220c

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
@@ -37,5 +37,6 @@ public class User extends UserData implements com.bss.inc.redsmokes.api.IUser, C
 
     void update(final Player base) {setBase(base);}
     public IRedSmokes getRedSmokes() {return redsmokes;}
+    @Override
     
 }
