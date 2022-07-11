2f43166ed1a6e08c0632923cfc0c638950a44c4b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-12 02:57:58 +0300

    Update-5b042c4e-b225-4f3f-b61d-d2e3b767880f

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/BalanceTopImpl.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/BalanceTopImpl.java
@@ -1,7 +1,8 @@
 package com.bss.inc.redsmokes.main;
 
+import com.bss.inc.redsmokes.api.IRedSmokes;
 import com.bss.inc.redsmokes.api.services.BalanceTop;
 
 public class BalanceTopImpl implements BalanceTop {
-    
+    private final IRedSmokes
 }
