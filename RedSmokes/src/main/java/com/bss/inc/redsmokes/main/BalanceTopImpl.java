b7f9db1653f76ef8e96a762403b6695f19e409d0
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-12 02:58:40 +0300

    Update-f997c01d-1832-4edf-bdc0-b30392adec7f

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/BalanceTopImpl.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/BalanceTopImpl.java
@@ -6,10 +6,12 @@ import com.bss.inc.redsmokes.api.services.BalanceTop;
 import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class BalanceTopImpl implements BalanceTop {
     private final IRedSmokes redSmokes;
     private LinkedHashMap<UUID, BalanceTop.Entry> topCache = new LinkedHashMap<>();
     private BigDecimal balanceTopTotal = BigDecimal.ZERO;
     private long cacheAge = 0;
+    private CompletableFuture
 }
