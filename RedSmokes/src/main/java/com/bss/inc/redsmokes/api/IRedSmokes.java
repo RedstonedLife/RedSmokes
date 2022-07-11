717fad301e3924dccc4e6063287c416d8bc82656
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 06:04:14 +0300

    Update-c70ecaf4-b467-44b1-bae1-ac14fad0fc07

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/api/IRedSmokes.java
@@ -1,3 +1,39 @@
 package com.bss.inc.redsmokes.api;
 
-public interface IRedSmokes extends com.bss.inc.redsmokes.main.IRedSmokes {}
+import com.bss.inc.redsmokes.main.provider.PotionMetaProvider;
+import com.bss.inc.redsmokes.main.provider.SpawnEggProvider;
+
+import java.util.Collection;
+
+public interface IRedSmokes extends com.bss.inc.redsmokes.main.IRedSmokes {
+    /**
+     * Get a list of players who are vanished.
+     *
+     * @return A list of players who are vanished
+     */
+    Collection<String> getVanishedPlayersNew();
+
+    /**
+     * Get the spawn egg provider for the current platform.
+     *
+     * @return The current active spawn egg provider
+     */
+    SpawnEggProvider getSpawnEggProvider();
+
+    /**
+     * Get the potion meta provider for the current platform.
+     *
+     * @return The current active potion meta provider
+     */
+    PotionMetaProvider getPotionMetaProvider();
+
+    /**
+     * Get the {@link CustomItemResolver} that is currently in use.
+     *
+     * <b>Note: external plugins should generally avoid using this. If you want to add custom items from your plugin,
+     * you probably want to implement your own {@link net.ess3.api.IItemDb.ItemResolver}.</b>
+     *
+     * @return The custom item resolver
+     */
+    CustomItemResolver getCustomItemResolver();
+}
