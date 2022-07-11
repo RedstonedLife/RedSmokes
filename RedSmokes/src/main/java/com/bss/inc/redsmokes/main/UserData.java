a8b66624c655ca5a336551287077b8a079db1c9c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 03:53:10 +0300

    Update-443506f2-4e1f-4e63-9055-850dcf880752

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserData.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserData.java
@@ -2,6 +2,7 @@ package com.bss.inc.redsmokes.main;
 
 import com.bss.inc.redsmokes.api.IConf;
 import com.bss.inc.redsmokes.api.IRedSmokes;
+import com.bss.inc.redsmokes.main.config.RedSmokesUserConfiguration;
 
 public abstract class UserData extends PlayerExtension implements IConf {
     protected final transient IRedSmokes redsmokes;
