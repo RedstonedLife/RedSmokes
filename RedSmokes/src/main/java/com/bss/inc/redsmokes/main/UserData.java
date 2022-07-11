dfa8e2593fb9789473affbc37d7957123d536436
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-11 03:55:37 +0300

    Update-d373a349-9662-499e-a8ed-b0dabc37f027

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserData.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserData.java
@@ -19,5 +19,6 @@ public abstract class UserData extends PlayerExtension implements IConf {
         super(base);
         this.redsmokes = redsmokes;
         final File folder = new File(redsmokes.getDataFolder(), "userdata");
+        if(!folder.exists())
     }
 }
