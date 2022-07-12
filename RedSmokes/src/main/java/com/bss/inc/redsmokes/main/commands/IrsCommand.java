bc5f221e21987aefb960b08e10eaf8d466c3dfcb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-12 03:33:25 +0300

    Update-fd1fbb77-ce24-4caa-95ce-df7ede2f5a04

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/IrsCommand.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/IrsCommand.java
@@ -1,4 +1,32 @@
 package com.bss.inc.redsmokes.main.commands;
 
+import com.bss.inc.redsmokes.api.IRedSmokes;
+import com.bss.inc.redsmokes.main.CommandSource;
+import com.bss.inc.redsmokes.main.IRedSmokesModule;
+import com.bss.inc.redsmokes.main.User;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IrsCommand {
+    String getName();
+
+    Map<String, String> getUsageStrings();
+
+    void run(Server server, User user, String commandLabel, Command cmd, String[] args) throws Exception;
+
+    void run(Server server, CommandSource sender, String commandLabel, Command cmd, String[] args) throws Exception;
+
+    List<String> tabComplete(Server server, User user, String commandLabel, Command cmd, String[] args);
+
+    List<String> tabComplete(Server server, CommandSource sender, String commandLabel, Command cmd, String[] args);
+
+    void setEssentials(IRedSmokes ess);
+
+    void setEssentialsModule(IRedSmokesModule module);
+
+    void showError(CommandSender sender, Throwable throwable, String commandLabel);
 }
