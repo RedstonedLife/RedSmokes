48d70aa0df97ea720a290910e4a5a355df6c67cf
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-12 03:31:32 +0300

    Update-d8d0657e-2a47-4214-b426-7d6aa18d591b

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/api/commands/IrsCommand.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/api/commands/IrsCommand.java
@@ -1,4 +1,3 @@
 package com.bss.inc.redsmokes.api.commands;
 
-public interface IrsCommand extends com.bss.inc.redsmokes.main.commands.IrsCommand {
-}
+public interface IrsCommand extends com.bss.inc.redsmokes.main.commands.IrsCommand {}
