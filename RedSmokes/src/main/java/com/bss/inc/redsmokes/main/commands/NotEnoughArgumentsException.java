423b8c763bafd654c1d0062d6ee57750c918335e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-12 03:35:03 +0300

    Update-d09adb7f-c042-4ba0-9b99-3f3b3ab1e32d

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/NotEnoughArgumentsException.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/NotEnoughArgumentsException.java
@@ -1,4 +1,15 @@
 package com.bss.inc.redsmokes.main.commands;
 
-public class NotEnoughArgumentsException {
+public class NotEnoughArgumentsException extends Exception {
+    public NotEnoughArgumentsException() {
+        super("");
+    }
+
+    public NotEnoughArgumentsException(final String string) {
+        super(string);
+    }
+
+    public NotEnoughArgumentsException(final Throwable ex) {
+        super("", ex);
+    }
 }
