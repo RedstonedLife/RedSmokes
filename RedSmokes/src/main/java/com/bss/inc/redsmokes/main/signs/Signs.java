3dacc0894644d5448e48de3f65072f2afec7b1a3
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-12 04:41:50 +0300

    Update-72659b10-d7d9-4761-9b7d-16b8e9f5d9d5

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/signs/Signs.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/signs/Signs.java
@@ -3,5 +3,6 @@ package com.bss.inc.redsmokes.main.signs;
 public enum Signs {
     ;
     private final RedSmokesSign sign;
-    
+    Signs(final RedSmokesSign sign) {this.sign = sign;}
+    public RedSmokesSign getSign() {return sign;}
 }
