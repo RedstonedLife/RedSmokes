8b173b3bfae7335e9c5a90760b39123266906a2c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-12 03:23:24 +0300

    Update-fed0b78b-67f1-472c-b8c8-965792c00236

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/User.java
@@ -348,16 +348,6 @@ public class User extends UserData implements com.bss.inc.redsmokes.api.IUser, C
     }
 
     @Override
-    public void sendMail(MailSender sender, String message) {
-        sendMail(sender, message, 0);
-    }
-
-    @Override
-    public void sendMail(MailSender sender, String message, long expireAt) {
-        redsmokes.getMail().sendMail(this, sender, message, expireAt);
-    }
-
-    @Override
     public ArrayList<MailMessage> getMailMessages() {
         return null;
     }
