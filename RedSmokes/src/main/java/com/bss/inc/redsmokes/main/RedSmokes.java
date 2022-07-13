f0931bf841a314f43daa84f1746748999bc98c30
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 06:20:01 +0300

    Update-8c70183a-8c0c-43ef-9d0e-5cc7279a5cf4

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -650,11 +650,6 @@ public class RedSmokes extends JavaPlugin implements IRedSmokes {
 
             final CommandSource sender = new CommandSource(cSender);
 
-            // New mail notification
-            if (user != null && !getSettings().isCommandDisabled("mail") && !command.getName().equals("mail") && user.isAuthorized("essentials.mail")) {
-                user.notifyOfMail();
-            }
-
             //Print version even if admin command is not available #easteregg
             if (commandLabel.equalsIgnoreCase("essversion")) {
                 sender.sendMessage("This server is running Essentials " + getDescription().getVersion());
