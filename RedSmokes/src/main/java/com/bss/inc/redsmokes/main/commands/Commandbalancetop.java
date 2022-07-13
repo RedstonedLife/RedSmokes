150224c00de620a04c022c09f8ff05bd982c8759
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 19:47:46 +0300

    Update-9c075983-3f6f-4f88-9f87-bf9e54692329

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbalancetop.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbalancetop.java
@@ -1,5 +1,6 @@
 package com.bss.inc.redsmokes.main.commands;
 
 public class Commandbalancetop extends RedSmokesCommand {
-    
+    public static int MINUSERS = 50;
+    private static int CACHETIME = 2 * 60 * 1000;
 }
