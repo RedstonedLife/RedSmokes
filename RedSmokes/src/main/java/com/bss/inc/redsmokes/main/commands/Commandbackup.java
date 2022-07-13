d67cb56aca87deb532fb41bc8784a7e5c1614b90
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 22:10:36 +0300

    Update-9f453d7e-3d33-41e7-90d7-03ce7e9ec9e3

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbackup.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbackup.java
@@ -1,5 +1,11 @@
 package com.bss.inc.redsmokes.main.commands;
 
+import com.bss.inc.redsmokes.main.Backup;
+import com.bss.inc.redsmokes.main.CommandSource;
+import org.bukkit.Server;
+
+import static com.bss.inc.redsmokes.main.I18n.tl;
+
 public class Commandbackup extends RedSmokesCommand {
     public Commandbackup() {
         super("backup");
@@ -7,11 +13,11 @@ public class Commandbackup extends RedSmokesCommand {
 
     @Override
     protected void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
-        final Backup backup = ess.getBackup();
+        final Backup backup = redSmokes.getBackup();
         if (backup == null) {
             throw new Exception(tl("backupDisabled"));
         }
-        final String command = ess.getSettings().getBackupCommand();
+        final String command = redSmokes.getSettings().getBackupCommand();
         if (command == null || "".equals(command) || "save-all".equalsIgnoreCase(command)) {
             throw new Exception(tl("backupDisabled"));
         }
