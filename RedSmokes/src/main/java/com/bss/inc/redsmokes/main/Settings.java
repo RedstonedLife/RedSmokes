7e07ed71a5dfbc1dcea0e1c1e57be3a310b4be77
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 07:29:19 +0300

    Update-f0885b7c-6940-4912-adb9-44a9f40c8b19

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Settings.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Settings.java
@@ -73,6 +73,7 @@ public class Settings implements net.redsmokes.api.ISettings {
     private boolean allowOldIdSigns;
     private boolean isSafeUsermap;
 
+    
     public Settings(final @NotNull IRedSmokes redSmokes) {
         this.redSmokes = redSmokes;
         config = new RedSmokesConfiguration(new File(redSmokes.getDataFolder(), "config.yml"), "/config.yml");
