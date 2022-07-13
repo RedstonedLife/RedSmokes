0315cfe7a1bca7d2a623a48f72c5fccd026577b7
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 06:19:49 +0300

    Update-33bc686a-9d6e-4823-9464-b50e77a76f80

--- RedSmokes/src/main/java/net/redsmokes/api/ISettings.java
+++ RedSmokes/src/main/java/net/redsmokes/api/ISettings.java
@@ -4,4 +4,6 @@ import java.util.List;
 
 public interface ISettings extends com.bss.inc.redsmokes.main.ISettings {
     boolean isCommandCooldownsEnabled();
+
+    boolean logCommandBlockCommands();
 }
