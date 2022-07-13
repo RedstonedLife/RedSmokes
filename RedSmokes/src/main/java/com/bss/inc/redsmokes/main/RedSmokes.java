718a738d1f3ca100e8be93db3c5f0d69c4697f51
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 04:56:14 +0300

    Update-efdfb8d9-0dc9-4ff5-b350-d634b24b33ab

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -205,7 +205,9 @@ public class RedSmokes extends JavaPlugin implements IRedSmokes {
              * Item #1 - Date Information (When it was sent)
              * Item #2 - User Information (From Who: To Who:)
              * Item #3 - Digital Signature / Location Signature (If provider paid for them)
-             * Item #4 - Reply
+             * Item #4 - Reply To Mail
+             * Item #5 - View Content (Book & Quil GUI)
+             * Item #6 - Delete Mail
              */
 
         } catch (final NumberFormatException ex) {
