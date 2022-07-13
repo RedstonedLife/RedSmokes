dd21c9aeb24aee37556de263270c2af465371f4a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 08:17:50 +0300

    Update-80f07578-85a5-4744-bbfe-81ef5443d7fc

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -228,7 +228,7 @@ public class RedSmokes extends JavaPlugin implements IRedSmokes {
              * Note, Made by Tal A. Baskin (13th of July, 2022. 4:56 AM)
              * If at some point I'll have enough time, I'll try to actually make this a SMTP Mail Manager so theoretically I could give it a domain I own
              * for example, example.com. And users/admins can register a mail under it, so if an admin wants to send an email (I will add a discord-interpreter)
-             * so users could register an email from the discord, like so /register-email <br>USERNAME</br> <br>PASSWORD</br> (In dms for safety)
+             * so users could register an email from the discord, like so /register-email <b>USERNAME</b> <b>PASSWORD</b> (In dms for safety)
              * and send mails to actual mail services like gmail, hotmail, protonmail just to name a few. If I could actually implement this
              * I would offer hosting for the SMTP side on my servers to off-load servers.
              */
