64e83755bebb859ed38a9efd8ac21506ad027887
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 19:52:35 +0300

    Update-2d4ad22a-d705-47d2-bb52-1e79c72f42d1

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbalancetop.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbalancetop.java
@@ -25,4 +25,85 @@ public class Commandbalancetop extends RedSmokesCommand {
         sender.sendMessage(tl("balanceTop", format.format(cal.getTime())));
         new TextPager(cache).showPage(Integer.toString(page), null, "balancetop", sender);
     }
+
+    @Override
+    protected void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
+        int page = 0;
+        boolean force = false;
+        if (args.length > 0) {
+            try {
+                page = Integer.parseInt(args[0]);
+            } catch (final NumberFormatException ex) {
+                if (args[0].equalsIgnoreCase("force") && (!sender.isPlayer() || ess.getUser(sender.getPlayer()).isAuthorized("essentials.balancetop.force"))) {
+                    force = true;
+                }
+            }
+        }
+
+        if (!force && ess.getBalanceTop().getCacheAge() > System.currentTimeMillis() - CACHETIME) {
+            outputCache(sender, page);
+            return;
+        }
+
+        // If there are less than 50 users in our usermap, there is no need to display a warning as these calculations should be done quickly
+        if (ess.getUserMap().getUniqueUsers() > MINUSERS) {
+            sender.sendMessage(tl("orderBalances", ess.getUserMap().getUniqueUsers()));
+        }
+
+        ess.runTaskAsynchronously(new Viewer(sender, page, force));
+    }
+
+    @Override
+    protected List<String> getTabCompleteOptions(final Server server, final CommandSource sender, final String commandLabel, final String[] args) {
+        if (args.length == 1) {
+            final List<String> options = Lists.newArrayList("1");
+            if (!sender.isPlayer() || ess.getUser(sender.getPlayer()).isAuthorized("essentials.balancetop.force")) {
+                options.add("force");
+            }
+            return options;
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    private class Viewer implements Runnable {
+        private final transient CommandSource sender;
+        private final transient int page;
+        private final transient boolean force;
+
+        Viewer(final CommandSource sender, final int page, final boolean force) {
+            this.sender = sender;
+            this.page = page;
+            this.force = force;
+        }
+
+        @Override
+        public void run() {
+            if (ess.getSettings().isEcoDisabled()) {
+                if (ess.getSettings().isDebug()) {
+                    ess.getLogger().info("Internal economy functions disabled, aborting baltop.");
+                }
+                return;
+            }
+
+            final boolean fresh = force || ess.getBalanceTop().isCacheLocked() || ess.getBalanceTop().getCacheAge() <= System.currentTimeMillis() - CACHETIME;
+            final CompletableFuture<Void> future = fresh ? ess.getBalanceTop().calculateBalanceTopMapAsync() : CompletableFuture.completedFuture(null);
+            future.thenRun(() -> {
+                if (fresh) {
+                    final SimpleTextInput newCache = new SimpleTextInput();
+                    newCache.getLines().add(tl("serverTotal", NumberUtil.displayCurrency(ess.getBalanceTop().getBalanceTopTotal(), ess)));
+                    int pos = 1;
+                    for (final Map.Entry<UUID, BalanceTop.Entry> entry : ess.getBalanceTop().getBalanceTopCache().entrySet()) {
+                        if (ess.getSettings().showZeroBaltop() || entry.getValue().getBalance().compareTo(BigDecimal.ZERO) > 0) {
+                            newCache.getLines().add(tl("balanceTopLine", pos, entry.getValue().getDisplayName(), NumberUtil.displayCurrency(entry.getValue().getBalance(), ess)));
+                        }
+                        pos++;
+                    }
+                    cache = newCache;
+                }
+
+                outputCache(sender, page);
+            });
+        }
+    }
 }
