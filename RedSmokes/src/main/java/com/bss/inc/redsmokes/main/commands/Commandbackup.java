49d9782f72e79e7b0f9252b96ec06da9e2d30cd0
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 22:10:21 +0300

    Update-ea4bbd01-1995-41ce-ad7b-52d3a0d9cfe7

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbackup.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbackup.java
@@ -1,4 +1,21 @@
 package com.bss.inc.redsmokes.main.commands;
 
-public class Commandbackup {
+public class Commandbackup extends RedSmokesCommand {
+    public Commandbackup() {
+        super("backup");
+    }
+
+    @Override
+    protected void run(final Server server, final CommandSource sender, final String commandLabel, final String[] args) throws Exception {
+        final Backup backup = ess.getBackup();
+        if (backup == null) {
+            throw new Exception(tl("backupDisabled"));
+        }
+        final String command = ess.getSettings().getBackupCommand();
+        if (command == null || "".equals(command) || "save-all".equalsIgnoreCase(command)) {
+            throw new Exception(tl("backupDisabled"));
+        }
+        backup.run();
+        sender.sendMessage(tl("backupStarted"));
+    }
 }
