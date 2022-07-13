087b3851cb072768681ab0a943bfae3cce7683c1
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 09:53:24 +0300

    Update-7b2ec402-6376-4234-b6a3-5e99106048f2

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandredsmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandredsmokes.java
@@ -1,4 +1,11 @@
 package com.bss.inc.redsmokes.main.commands;
 
-public class Commandredsmokes {
+import com.bss.inc.redsmokes.main.utils.EnumUtil;
+import org.bukkit.Sound;
+
+public class Commandredsmokes extends RedSmokesCommand {
+
+    private static final Sound NOTE_HARP = EnumUtil.valueOf(Sound.class, "BLOCK_NOTE_BLOCK_HARP", "BLOCK_NOTE_HARP", "NOTE_PIANO");
+    private static final Sound MOO_SOUND = EnumUtil.valueOf(Sound.class, "COW_IDLE", "ENTITY_COW_MILK");
+
 }
