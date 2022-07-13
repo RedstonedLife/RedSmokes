492f5f81a01634ec4b68362a569d962a7b396016
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 11:26:28 +0300

    Update-96094a99-e2cc-4aaf-b774-600a6a5bc9e2

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandredsmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandredsmokes.java
@@ -2,11 +2,16 @@ package com.bss.inc.redsmokes.main.commands;
 
 import com.bss.inc.redsmokes.main.CommandSource;
 import com.bss.inc.redsmokes.main.utils.EnumUtil;
+import com.google.common.collect.ImmutableMap;
 import org.bukkit.Server;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Commandredsmokes extends RedSmokesCommand {
 
@@ -50,6 +55,62 @@ public class Commandredsmokes extends RedSmokesCommand {
 
     private void showUsage(final CommandSource sender) throws Exception {
         throw new NotEnoughArgumentsException();
+    }
+
+    private static class TuneRunnable extends BukkitRunnable {
+        private static final Map<String, Float> noteMap = ImmutableMap.<String, Float>builder()
+                .put("1F#", 0.5f)
+                .put("1G", 0.53f)
+                .put("1G#", 0.56f)
+                .put("1A", 0.6f)
+                .put("1A#", 0.63f)
+                .put("1B", 0.67f)
+                .put("1C", 0.7f)
+                .put("1C#", 0.76f)
+                .put("1D", 0.8f)
+                .put("1D#", 0.84f)
+                .put("1E", 0.9f)
+                .put("1F", 0.94f)
+                .put("2F#", 1.0f)
+                .put("2G", 1.06f)
+                .put("2G#", 1.12f)
+                .put("2A", 1.18f)
+                .put("2A#", 1.26f)
+                .put("2B", 1.34f)
+                .put("2C", 1.42f)
+                .put("2C#", 1.5f)
+                .put("2D", 1.6f)
+                .put("2D#", 1.68f)
+                .put("2E", 1.78f)
+                .put("2F", 1.88f)
+                .build();
+
+        private final String[] tune;
+        private final Sound sound;
+        private final Supplier<Collection<Player>> players;
+        private int i = 0;
+
+        TuneRunnable(final String tuneStr, final Sound sound, final Supplier<Collection<Player>> players) {
+            this.tune = tuneStr.split(",");
+            this.sound = sound;
+            this.players = players;
+        }
+
+        @Override
+        public void run() {
+            final String note = tune[i];
+            i++;
+            if (i >= tune.length) {
+                cancel();
+            }
+            if (note == null || note.isEmpty()) {
+                return;
+            }
+
+            for (final Player onlinePlayer : players.get()) {
+                onlinePlayer.playSound(onlinePlayer.getLocation(), sound, 1, noteMap.get(note));
+            }
+        }
     }
 
 }
