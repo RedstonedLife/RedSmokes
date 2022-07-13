a17a4d1cb4c3d6bbb70abc4043fc567cdda999ed
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 06:54:48 +0300

    Update-1fa806b8-38d0-48ed-8259-086a25abfe2b

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/IUser.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/IUser.java
@@ -17,7 +17,7 @@ public interface IUser {
     boolean isPermissionSet(String node);
     /**
         RedSmokes Balance (IF RedSmokes is not found)
-        Will be deprecated in the future included in <br>SoyuzCore</br> and will be imported from its API.
+        Will be deprecated in the future included in <b>SoyuzCore</b> and will be imported from its API.
 
         <h3>SoyuzCore</h3>
         SoyuzCore is a private project by <p href="https://github.com/RedstonedLife">Tal A. Baskin</p> as part of a
