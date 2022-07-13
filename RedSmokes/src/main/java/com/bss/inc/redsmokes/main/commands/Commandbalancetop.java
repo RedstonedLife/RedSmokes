e778d7db2f5dcc2d6efb6750d57a1552ca6aa49a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 19:51:10 +0300

    Update-3ca80760-0d86-494f-b70f-5a416dc8bb63

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbalancetop.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/commands/Commandbalancetop.java
@@ -1,6 +1,12 @@
 package com.bss.inc.redsmokes.main.commands;
 
+import com.bss.inc.redsmokes.main.CommandSource;
 import com.bss.inc.redsmokes.main.textreader.SimpleTextInput;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+import static com.bss.inc.redsmokes.main.I18n.tl;
 
 public class Commandbalancetop extends RedSmokesCommand {
     public static int MINUSERS = 50;
@@ -11,5 +17,11 @@ public class Commandbalancetop extends RedSmokesCommand {
         super("balancetop");
     }
 
-    private void 
+    private void outputCache(final CommandSource sender, final int page) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(redSmokes.getBalanceTop().getCacheAge());
+        final DateFormat format = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+        sender.sendMessage(tl("balanceTop", format.format(cal.getTime())));
+        new TextPager(cache).showPage(Integer.toString(page), null, "balancetop", sender);
+    }
 }
