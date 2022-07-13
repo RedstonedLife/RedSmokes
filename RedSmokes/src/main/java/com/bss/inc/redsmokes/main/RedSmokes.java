3c73166b82183c9c6ffdc65b5da4da60de6bbe85
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 06:22:14 +0300

    Update-320b309b-3955-4a84-b13a-5aaebffea387

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -736,10 +736,13 @@ public class RedSmokes extends JavaPlugin implements IRedSmokes {
         pm.registerEvents(new Listener() {
             @EventHandler(priority = EventPriority.LOW)
             public void onPlayerJoin(final PlayerJoinEvent event) {
-                event.getPlayer().sendMessage("RedSmokes failed to load, read the log file");
+                event.getPlayer().sendMessage("Essentials failed to load, read the log file.");
             }
         }, this);
-        //for(final Player player : getOn)
+        for (final Player player : getOnlinePlayers()) {
+            player.sendMessage("Essentials failed to load, read the log file.");
+        }
+        this.setEnabled(false);
     }
 
     private boolean isRedSmokesPlugin(Plugin plugin) {
