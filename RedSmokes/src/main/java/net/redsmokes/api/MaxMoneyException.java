82ee4674c03507a71525d00ed006cc7653b8ab44
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 06:31:37 +0300

    Update-d0f44d4c-5bcf-410e-993b-70d93411e88a

--- RedSmokes/src/main/java/net/redsmokes/api/MaxMoneyException.java
+++ RedSmokes/src/main/java/net/redsmokes/api/MaxMoneyException.java
@@ -1,7 +1,9 @@
 package net.redsmokes.api;
 
+import static com.bss.inc.redsmokes.main.I18n.tl;
+
 public class MaxMoneyException extends Exception {
     public MaxMoneyException() {
-        //super(tl("maxMoney"));
+        super(tl("maxMoney"));
     }
 }
