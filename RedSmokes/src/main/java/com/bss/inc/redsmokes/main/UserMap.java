2f042d2b4df10fb13908b5c8817a9fc1177e0dd8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 05:15:10 +0300

    Update-01f7cfdb-abff-4229-acc4-a9ff6cb429a7

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserMap.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/UserMap.java
@@ -1,6 +1,7 @@
 package com.bss.inc.redsmokes.main;
 
 import com.bss.inc.redsmokes.OfflinePlayer;
+import net.redsmokes.api.IConf;
 import net.redsmokes.api.MaxMoneyException;
 import com.bss.inc.redsmokes.main.api.UserDoesNotExistException;
 import com.bss.inc.redsmokes.main.utils.StringUtil;
