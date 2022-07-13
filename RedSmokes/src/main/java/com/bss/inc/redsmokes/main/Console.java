7d4bc0941479bfa694f798a552663d78ad497628
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 03:39:02 +0300

    Update-b7a469dd-4ea2-484e-b028-ad439ea760a5

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Console.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/Console.java
@@ -3,5 +3,6 @@ package com.bss.inc.redsmokes.main;
 import com.bss.inc.redsmokes.main.messaging.IMessageRecipient;
 
 public final class Console implements IMessageRecipient {
-    public static final String NAME = ""
+    public static final String NAME = "Console";
+    public static final 
 }
