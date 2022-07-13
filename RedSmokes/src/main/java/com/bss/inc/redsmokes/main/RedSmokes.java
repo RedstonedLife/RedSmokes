f11cb83fc274dcbb239668dbd6dad01742510ae9
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-13 12:12:18 +0300

    Update-c2f8a2b8-52cb-49d5-8a68-050e92972608

--- RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
+++ RedSmokes/src/main/java/com/bss/inc/redsmokes/main/RedSmokes.java
@@ -617,7 +617,7 @@ public class RedSmokes extends JavaPlugin implements IRedSmokes {
     @Override
     public boolean onCommandRedSmokes(final CommandSender cSender, final Command command, final String commandLabel, final String[] args, final ClassLoader classLoader, final String commandPath, final String permissionPrefix, final IRedSmokesModule module) {
         // Allow plugins to override the command via onCommand
-        if (!getSettings().isCommandOverridden(command.getName()) && (!commandLabel.startsWith("e") || commandLabel.equalsIgnoreCase(command.getName()))) {
+        if (!getSettings().isCommandOverridden(command.getName()) && (!commandLabel.startsWith("r") || commandLabel.equalsIgnoreCase(command.getName()))) {
             if (getSettings().isDebug()) {
                 LOGGER.log(Level.INFO, "Searching for alternative to: " + commandLabel);
             }
